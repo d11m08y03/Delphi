13e2516384c4840aa8083fbba73b48bc8da7ee7e
d11m08y03 <zakariyyakurmally@gmail.com>
2025-06-02 19:48:05 +0400

    Add test route

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 from app.lifespan import app_lifespan
 from app.interfaces.middleware import add_middlewares
 from app.interfaces.controllers.oauth2_controller import router as google_oauth2_router
+from app.interfaces.controllers.test_controller import router as test_router 
 
 # Create the FastAPI instance
 app = FastAPI(
-    title="My Backend",
+    title="Delphi",
     description="OAuth2 Integration with FastAPI",
     version="1.0.0",
     lifespan=app_lifespan,
@@ -16,3 +17,4 @@ add_middlewares(app)
 
 # Include routes
 app.include_router(google_oauth2_router)
+app.include_router(test_router)
